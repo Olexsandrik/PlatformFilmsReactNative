7c3764be8066dc38324f9e191d2b58af372d194f
Olexsandrik <119703838+Olexsandrik@users.noreply.github.com>
2025-04-15 01:08:09 +0300

    update data

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+
+const _Layout = () => {
+  return (
+    <Tabs>
+      <Tabs.Screen />
+    </Tabs>
+  );
+};
+
+export default _Layout;
